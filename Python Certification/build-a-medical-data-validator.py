6ddcde4bc1b8e6eb8039462a433044c0531ce6ff
Vaishnavi Nayak <139222348+VaishnaviNayak2023@users.noreply.github.com>
2026-02-14 14:05:53 +0530

    Create build-a-medical-data-validator.py

---
+++ Python Certification/build-a-medical-data-validator.py
@@ -0,0 +1,36 @@
+def validate(data):
+    is_sequence = isinstance(data, (list, tuple))
+
+    if not is_sequence:
+        print('Invalid format: expected a list or tuple.')
+        return False
+        
+    is_invalid = False
+    key_set = set(
+        ['patient_id', 'age', 'gender', 'diagnosis', 'medications', 'last_visit_id']
+    )
+
+    for index, dictionary in enumerate(data):
+        if not isinstance(dictionary, dict):
+            print(f'Invalid format: expected a dictionary at position {index}.')
+            is_invalid = True
+            continue
+
+        if set(dictionary.keys()) != key_set:
+            print(
+                f'Invalid format: {dictionary} at position {index} has missing and/or invalid keys.'
+            )
+            is_invalid = True
+            continue
+
+        invalid_records = find_invalid_records(**dictionary)
+
+        # <-- Add this loop
+        for key in invalid_records:
+            print(f"Unexpected format '{key}: {dictionary[key]}' at position {index}.")
+            is_invalid = True
+
+    if is_invalid:
+        return False
+    print('Valid format.')
+    return True
